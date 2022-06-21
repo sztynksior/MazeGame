d297b0c52f99d6eafff40a52b84e426ecf483ec1
sztynksior <69170918+sztynksior@users.noreply.github.com>
2022-06-21 11:12:54 +0200

    added menu class

---
+++ MazeGame/MazeGame/Menu.h
@@ -0,0 +1,26 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include "TextureMenager.h"
+#include <SFML/Graphics.hpp>
+#include <utility>
+
+#define MENU_BUTTONS 5
+class Menu : sf::Drawable
+{
+private:
+	enum class whichButton {PLAY, EXIT, EASY, MEDIUM, HARD};
+
+	std::vector<std::pair<sf::Sprite, sf::Sprite>> menuButtons;
+
+public:
+
+	void setTextures(TextureMenager& pTextureMenager);
+
+};
+
+#endif // !MENU_H
+
+
+
+
