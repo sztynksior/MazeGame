54d59aee45415f5e8b8180173bb0d5dde0b9a73f
sztynksior <69170918+sztynksior@users.noreply.github.com>
2022-06-21 11:37:13 +0200

    menu changed setTextures method

--- MazeGame/MazeGame/Menu.cpp
+++ MazeGame/MazeGame/Menu.cpp
@@ -2,18 +2,11 @@
 
 void Menu::setTextures(TextureMenager& pTextureMenager)
 {
-	this->menuButtons[PLAY].first.setTexture(pTextureMenager.getTextureWithGivenName("PlayButton"));
-	this->menuButtons[PLAY].second.setTexture(pTextureMenager.getTextureWithGivenName("SelectedPlayButton"));
+	this->menuButtonsTextures.push_back({ pTextureMenager.getTextureWithGivenName("EasyBytton") ,pTextureMenager.getTextureWithGivenName("SelectedEasyBytton") });
 
-	this->menuButtons[EXIT].first.setTexture(pTextureMenager.getTextureWithGivenName("ExitButton"));
-	this->menuButtons[EXIT].second.setTexture(pTextureMenager.getTextureWithGivenName("SelectedExitButton"));
+	this->menuButtonsTextures.push_back({ pTextureMenager.getTextureWithGivenName("MediumButton") ,pTextureMenager.getTextureWithGivenName("SelectedMediumButton") });
 
-	this->menuButtons[EASY].first.setTexture(pTextureMenager.getTextureWithGivenName("EasyButton"));
-	this->menuButtons[EASY].second.setTexture(pTextureMenager.getTextureWithGivenName("SelectedEasyButton"));
+	this->menuButtonsTextures.push_back({ pTextureMenager.getTextureWithGivenName("HardButton") ,pTextureMenager.getTextureWithGivenName("SelectedHardButton") });
 
-	this->menuButtons[MEDIUM].first.setTexture(pTextureMenager.getTextureWithGivenName("MediumButton"));
-	this->menuButtons[MEDIUM].second.setTexture(pTextureMenager.getTextureWithGivenName("SelectedMediumButton"));
-
-	this->menuButtons[HARD].first.setTexture(pTextureMenager.getTextureWithGivenName("HardButton"));
-	this->menuButtons[HARD].second.setTexture(pTextureMenager.getTextureWithGivenName("SelectedHardButton"));
+	this->menuButtonsTextures.push_back({ pTextureMenager.getTextureWithGivenName("ExitButton") ,pTextureMenager.getTextureWithGivenName("SelectedExitButton") });
 }
